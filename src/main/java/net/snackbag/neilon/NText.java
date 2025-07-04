e1bf7d515a8df337df5c4ee15bbadd2c7fbb26f9
JXSnack <jx.snack@gmail.com>
2025-07-04 16:48:40 +0200

    QOL hover methods

--- src/main/java/net/snackbag/neilon/NText.java
+++ src/main/java/net/snackbag/neilon/NText.java
@@ -12,6 +12,18 @@ public class NText {
         return assemble().text(content).color(color).build();
     }
 
+    public static Text of(String content, String hover) {
+        return assemble().text(content).hover(hover).build();
+    }
+
+    public static Text of(String content, VColor color, String hover) {
+        return assemble().text(content).color(color).hover(hover).build();
+    }
+
+    public static Text of(String content, VColor color, String hover, VColor hoverColor) {
+        return assemble().text(content).color(color).hover(hover).hover(NText.of(hover, hoverColor)).build();
+    }
+
     public static TextAssembler assemble() {
         return new TextAssembler();
     }
