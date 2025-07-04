c51e7ea0f4ef0669386a26e5f64dc72278e37c03
JXSnack <jx.snack@gmail.com>
2025-07-04 16:22:18 +0200

    add of with color

--- src/main/java/net/snackbag/neilon/NText.java
+++ src/main/java/net/snackbag/neilon/NText.java
@@ -1,10 +1,15 @@
 package net.snackbag.neilon;
 
 import net.minecraft.text.Text;
+import net.snackbag.vera.core.VColor;
 
 public class NText {
     public static Text of(String content) {
         return assemble().text(content).build();
+    }
+
+    public static Text of(String content, VColor color) {
+        return assemble().text(content).color(color).build();
     }
 
     public static TextAssembler assemble() {
