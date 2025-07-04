36f60bf2efb207c4a3ad1344641d657cefe6ddfd
JXSnack <jx.snack@gmail.com>
2025-07-04 21:43:37 +0200

    add keybinding qol

--- src/main/java/net/snackbag/neilon/NText.java
+++ src/main/java/net/snackbag/neilon/NText.java
@@ -1,6 +1,7 @@
 package net.snackbag.neilon;
 
 import net.minecraft.text.Text;
+import net.snackbag.neilon.types.TextType;
 import net.snackbag.vera.core.VColor;
 
 public class NText {
@@ -10,6 +11,14 @@ public class NText {
 
     public static Text of(String content, VColor color) {
         return assemble().text(content).color(color).build();
+    }
+
+    public static Text keybinding(String key) {
+        return assemble().text(key, TextType.KEYBINDING).build();
+    }
+
+    public static Text keybinding(String key, VColor color) {
+        return assemble().text(key, TextType.KEYBINDING).color(color).build();
     }
 
     public static TextAssembler assemble() {
