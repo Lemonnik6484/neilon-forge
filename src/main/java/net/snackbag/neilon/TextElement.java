d089af2e2c0b8969b0f5db271a7ce3cf6766d4da
JXSnack <jx.snack@gmail.com>
2025-07-04 16:21:10 +0200

    add style system and color

--- src/main/java/net/snackbag/neilon/TextElement.java
+++ src/main/java/net/snackbag/neilon/TextElement.java
@@ -1,21 +1,30 @@
 package net.snackbag.neilon;
 
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.snackbag.vera.core.VColor;
 
 public class TextElement {
     public final String value;
     public final NType type;
+
+    public VColor color = VColor.white();
 
     protected TextElement(String value, NType type) {
         this.value = value;
         this.type = type;
     }
 
+    public Text applyStyles(MutableText text) {
+        text.styled(style -> style.withColor(color.toInt()));
+        return text;
+    }
+
     public Text convert() {
-        return switch (type) {
+        return applyStyles(switch (type) {
             case LITERAL -> Text.literal(value);
             case TRANSLATION -> Text.translatable(value);
             case KEYBINDING -> Text.keybind(value);
-        };
+        });
     }
 }
