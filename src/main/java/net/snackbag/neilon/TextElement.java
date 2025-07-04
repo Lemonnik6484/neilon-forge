491e23aa00ac4dbd76a835acbc950384eb74ca15
JXSnack <jx.snack@gmail.com>
2025-07-04 17:24:04 +0200

    add click actions

--- src/main/java/net/snackbag/neilon/TextElement.java
+++ src/main/java/net/snackbag/neilon/TextElement.java
@@ -1,8 +1,10 @@
 package net.snackbag.neilon;
 
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.snackbag.neilon.types.ClickType;
 import net.snackbag.neilon.types.TextType;
 import net.snackbag.vera.core.VColor;
 import org.jetbrains.annotations.Nullable;
@@ -16,6 +18,9 @@ public class TextElement {
     public @Nullable Text hoverText;
     public @Nullable HoverEvent.ItemStackContent hoverItem;
     public @Nullable HoverEvent.EntityContent hoverEntity;
+
+    public @Nullable ClickType clickType;
+    public @Nullable String clickAction;
 
     protected TextElement(String value, TextType type) {
         this.value = value;
@@ -37,10 +42,35 @@ public class TextElement {
                 style = style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ENTITY, hoverEntity));
             }
 
+            //
+            // Click actions
+            //
+
+            potentiallyFixCommand();
+
+            if (clickType != null) {
+                switch (clickType) {
+                    case RUN -> style = style.withClickEvent(createClickEvent(ClickEvent.Action.RUN_COMMAND));
+                    case URL -> style = style.withClickEvent(createClickEvent(ClickEvent.Action.OPEN_URL));
+                    case SUGGEST -> style = style.withClickEvent(createClickEvent(ClickEvent.Action.SUGGEST_COMMAND));
+                    case COPY -> style = style.withClickEvent(createClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD));
+                }
+            }
+
             return style;
         });
 
         return text;
+    }
+
+    private void potentiallyFixCommand() {
+        if (clickType == ClickType.RUN && !clickAction.startsWith("/")) {
+            clickAction = "/" + clickAction;
+        }
+    }
+
+    private ClickEvent createClickEvent(ClickEvent.Action action) {
+        return new ClickEvent(action, clickAction);
     }
 
     public Text convert() {
