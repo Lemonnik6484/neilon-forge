858a3a564096235c42647608fd5de03b27f83b4c
JXSnack <jx.snack@gmail.com>
2025-07-04 17:34:24 +0200

    add testing for click events

--- src/main/java/net/snackbag/neilon/impl/test/InternalTesting.java
+++ src/main/java/net/snackbag/neilon/impl/test/InternalTesting.java
@@ -7,7 +7,9 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.item.Items;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.snackbag.neilon.NText;
+import net.snackbag.neilon.types.ClickType;
 import net.snackbag.neilon.types.TextType;
 import net.snackbag.vera.core.VColor;
 
@@ -60,6 +62,36 @@ public class InternalTesting {
         source.sendMessage(NText.assemble()
                 .text("self hover")
                 .hover(source.getPlayer())
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click suggestion")
+                .click(ClickType.SUGGEST, "hello there")
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click run + fix")
+                .click(ClickType.RUN, "say hi")
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click run + no fix")
+                .click(ClickType.RUN, "/say ello")
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click open url + fix")
+                .click(ClickType.URL, "github.com/snackbag/neilon")
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click open url + no fix")
+                .click(ClickType.URL, "https://github.com/snackbag/neilon")
+                .build());
+
+        source.sendMessage(NText.assemble()
+                .text("click copy")
+                .click(ClickType.COPY, ":p")
                 .build());
 
         return 1;
