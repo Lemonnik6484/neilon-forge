f43c49cc65a7f0e6ef08a069be13f47378c2e870
JXSnack <jx.snack@gmail.com>
2025-07-04 16:02:16 +0200

    internal testing

--- src/main/java/net/snackbag/neilon/impl/NeilonMod.java
+++ src/main/java/net/snackbag/neilon/impl/NeilonMod.java
@@ -2,6 +2,8 @@ package net.snackbag.neilon.impl;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.snackbag.neilon.impl.test.InternalTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +15,7 @@ public class NeilonMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Neilon is installed");
+
+		if (FabricLoader.getInstance().isDevelopmentEnvironment()) InternalTesting.init();
 	}
 }
---
+++ src/main/java/net/snackbag/neilon/impl/test/InternalTesting.java
@@ -0,0 +1,37 @@
+package net.snackbag.neilon.impl.test;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.snackbag.neilon.NText;
+import net.snackbag.neilon.NType;
+
+public class InternalTesting {
+    public static void init() {
+        CommandRegistrationCallback.EVENT.register(InternalTesting::command);
+    }
+
+    private static void command(CommandDispatcher<ServerCommandSource> dispatcher,
+                                CommandRegistryAccess registryAccess,
+                                CommandManager.RegistrationEnvironment environment) {
+        dispatcher.register(CommandManager.literal("neilon")
+                .then(CommandManager.literal("test").executes(InternalTesting::executeTests)));
+    }
+
+    private static int executeTests(CommandContext<ServerCommandSource> ctx) {
+        var source = ctx.getSource();
+
+        source.sendMessage(NText.of("basic text"));
+
+        source.sendMessage(NText.assemble()
+                .text("block.minecraft.grass_block", NType.KEYBINDING)
+                .text("<-- keybinding (& stacked element)")
+                .build()
+        );
+
+        return 1;
+    }
+}
